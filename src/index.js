99ef8407ae4fef814716241ef01185016d98abe0
revuwem <revuwem@gmail.com>
2020-09-15 14:48:53 +0300

    action creators binded with dispatch

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
-import {inc, dec, random} from './actions';
+import * as actions from './actions';
 
 const store = createStore(reducer);
+const { dispatch } = store;
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// };
+
+let {inc, dec, random} = bindActionCreators(actions, dispatch);
 
 document
   .getElementById('increment')
-  .addEventListener('click', () => {
-    store.dispatch(inc());
-  });
+  .addEventListener('click', inc);
 
 document
   .getElementById('decrement')
-  .addEventListener('click', () => {
-    store.dispatch(dec());
-  });
+  .addEventListener('click', dec);
 
 document
   .getElementById('random')
   .addEventListener('click', () => {
 
     const payload = Math.floor(Math.random() * 10);
-    store.dispatch(random(payload));
+    random(payload);
   });
 
 const update = () => {
