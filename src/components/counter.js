21b03883159afebdecaf8b1bf6ed893f5a7269ba
revuwem <revuwem@gmail.com>
2020-09-16 10:13:20 +0300

    component connected with redux store

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const Counter = ({counter, inc, dec, random}) => {
+const Counter = ({ counter, inc, dec, random }) => {
 
     return (
         <div className="jumbotron">
@@ -9,13 +12,13 @@ const Counter = ({counter, inc, dec, random}) => {
             <h2>{counter}</h2>
             <br />
             <div className="d-flex align-items-center">
-                <button 
+                <button
                     className="btn btn-info mr-1"
                     onClick={dec}>Decrement</button>
-                <button 
+                <button
                     className="btn btn-success mr-1"
                     onClick={inc}>Increment</button>
-                <button 
+                <button
                     className="btn btn-primary mr-1"
                     onClick={random}>Random</button>
             </div>
@@ -23,4 +26,10 @@ const Counter = ({counter, inc, dec, random}) => {
     );
 };
 
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        counter: state
+    };
+};
+
+export default connect(mapStateToProps, actions)(Counter);
